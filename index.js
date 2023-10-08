fa5c1dc330057573fae671919ede26ee9aa545b7
Jacques RUDLOFF <jacques.rudloff@gmail.com>
2023-10-08 11:03:19 +0200

    manue website
    added bluring loading hero banner + small responsive changes

---
+++ index.js
@@ -0,0 +1,14 @@
+const imageIsloaded = (src, element) => {
+  const bgElement = document.querySelector(element);
+  let preloaderImg = document.createElement('img');
+  preloaderImg.src = src;
+  preloaderImg.addEventListener('load', (event) => {
+    bgElement.style.backgroundImage = `url(${src})`;
+    preloaderImg = null;
+  });
+};
+
+imageIsloaded(
+  'assets/images/background/full/backgroundHeroBanner.jpg',
+  '#hero-banner-section'
+);
