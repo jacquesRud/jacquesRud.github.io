a5f9ff7c16b4ccb5b8c34cffb54a758d0a201830
Jacques RUDLOFF <jacques.rudloff@gmail.com>
2023-11-18 15:59:57 +0100

    feature
    add actions badges

--- index.js
+++ index.js
@@ -12,3 +12,25 @@ imageIsloaded(
   'assets/images/background/full/backgroundHeroBanner.jpg',
   '#home'
 );
+
+const sendEmail = () => {
+  const recipientEmail = 'manuuu-e@hotmail.fr';
+  const subject = 'Prise de contact Emanuelle rudloff';
+
+  const body = `Bonjour, je viens de voir votre résumé et j'aimerai vous contacter`;
+
+  const mailtoLink =
+    'mailto:' +
+    recipientEmail +
+    '?subject=' +
+    encodeURIComponent(subject) +
+    '&body=' +
+    encodeURIComponent(body);
+
+  window.location.href = mailtoLink;
+};
+
+const downloadCV = () => {
+  const cvFilePath = 'assets/documents/CV Emmanuelle Rudloff.pdf';
+  window.open(cvFilePath, '_blank');
+};
